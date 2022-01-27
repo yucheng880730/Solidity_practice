e91e368eca55d63e4fac38ac48b9a5b5233b50a6
yucheng880730 <yucheng2k13@gmail.com>
2022-01-27 22:37:12 +0800

    2022/01/27 session3

--- zombie/zombiefeeding.sol
+++ zombie/zombiefeeding.sol
@@ -22,13 +22,27 @@ contract KittyInterface {
 
 contract ZombieFeeding is ZombieFactory {
 
-  address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
-  KittyInterface kittyContract = KittyInterface(ckAddress);
+  KittyInterface kittyContract;
 
-  function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) public {
+  // onlyOwner is a modifier
+  function setKittyContractAddress(address _address) external onlyOwner {
+    kittyContract =  KittyInterface(_address);
+  }
+
+  function _triggerCooldown(Zombie storage _zombie) internal {
+    _zombie.readyTime = uint32(now + cooldownTime);
+  }
+
+  function _isReady(Zombie storage _zombie) internal view returns (bool) {
+    return (_zombie.readyTime <= now);
+  }
+
+  function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) internal {
 
     require(msg.sender == zombieToOwner[_zombieId]);
     Zombie storage myZombie = zombies[_zombieId];
+
+    require(_isReady(myZombie));
 
      _targetDna = _targetDna % dnaModulus;
     uint newDna = (myZombie.dna + _targetDna) / 2;
@@ -38,7 +52,7 @@ contract ZombieFeeding is ZombieFactory {
     }
 
     _createZombie("NoName", newDna);
-
+    _triggerCooldown(myZombie);
   }
 
   // handle multiple return
